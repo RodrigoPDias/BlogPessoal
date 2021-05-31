bbc69ed39f6d56fab7c1a018ad6b375ebcf57a9e
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-05-31 11:47:13 -0300

    Update Postagem.java

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -38,6 +38,11 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
 	public Tema getTema() {
 		return tema;
 	}
